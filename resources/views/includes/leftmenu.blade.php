42997192e6f982b49d9cda07798f1791fcb892e6
Nishan Niriella <nishandilip12@gmail.com>
2021-09-17 23:58:24 +0530

    User_Auth_LeftMenuOptions_20210917

--- resources/views/includes/leftmenu.blade.php
+++ resources/views/includes/leftmenu.blade.php
@@ -26,8 +26,8 @@
                         class="avatar-status profile-status bg-green"></span>
                 </div>
                 <div class="user-info">
-                    <h4 class="font-weight-semibold mt-3 mb-0">{{'User Name'}}</h4>
-                    <span class="mb-0 text-muted">{{'Role'}}</span>
+                    <h4 class="font-weight-semibold mt-3 mb-0">{{ Auth::user()-> first_name }}</h4>
+                    <span class="mb-0 text-muted">{{ Auth::user()->role }}</span>
                 </div>
             </div>
         </div>
@@ -48,54 +48,56 @@
                     <li><a class="slide-item" href="{{route('payment_index')}}">Monthly Payment</a></li>
                 </ul>
             </li>
-            <li class="slide">
-                <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-book-open"></i>&nbsp;&nbsp;<span class="side-menu__label">WorkOut Plans</span><i class="angle fe fe-chevron-down"></i></a>
-                <ul class="slide-menu">
-                    <li><a class="slide-item" href="{{route('workout_plan_index')}}">WorkOut Plans</a></li>
-                    <li><a class="slide-item" href="{{url('/workout/workout_exercise')}}">WorkOut Exercises</a></li>
-                </ul>
-            </li>
-            <li class="slide">
-                <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-book-open"></i>&nbsp;&nbsp;<span class="side-menu__label">Diet Plans</span><i class="angle fe fe-chevron-down"></i></a>
-                <ul class="slide-menu">
-                    <li><a class="slide-item" href="{{route('diet_plan_index')}}">Diet Plans</a></li>
-                </ul>
-            </li>
-            <li class="slide">
-                <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-package"></i>&nbsp;&nbsp;<span class="side-menu__label">Package</span><i class="angle fe fe-chevron-down"></i></a>
-                <ul class="slide-menu">
-                    <li><a class="slide-item" href="{{route('package_index')}}">Active Packages</a></li>
-                    <li><a class="slide-item" href="{{route('package_view',['action' => 'Summary'])}}">Summary</a></li>
-                </ul>
-            </li>
-            <li class="slide">
-                <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-users"></i>&nbsp;&nbsp;<span class="side-menu__label">Users</span><i class="angle fe fe-chevron-down"></i></a>
-                <ul class="slide-menu">
-                    <li><a class="slide-item" href="#">Members</a></li>
+            @if (Auth::user()->role != 'Member')
+                <li class="slide">
+                    <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-book-open"></i>&nbsp;&nbsp;<span class="side-menu__label">WorkOut Plans</span><i class="angle fe fe-chevron-down"></i></a>
+                    <ul class="slide-menu">
+                        <li><a class="slide-item" href="{{route('workout_plan_index')}}">WorkOut Plans</a></li>
+                        <li><a class="slide-item" href="{{url('/workout/workout_exercise')}}">WorkOut Exercises</a></li>
+                    </ul>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-book-open"></i>&nbsp;&nbsp;<span class="side-menu__label">Diet Plans</span><i class="angle fe fe-chevron-down"></i></a>
+                    <ul class="slide-menu">
+                        <li><a class="slide-item" href="{{route('diet_plan_index')}}">Diet Plans</a></li>
+                    </ul>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-package"></i>&nbsp;&nbsp;<span class="side-menu__label">Package</span><i class="angle fe fe-chevron-down"></i></a>
+                    <ul class="slide-menu">
+                        <li><a class="slide-item" href="{{route('package_index')}}">Active Packages</a></li>
+                        <li><a class="slide-item" href="{{route('package_view',['action' => 'Summary'])}}">Summary</a></li>
+                    </ul>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-users"></i>&nbsp;&nbsp;<span class="side-menu__label">Users</span><i class="angle fe fe-chevron-down"></i></a>
+                    <ul class="slide-menu">
+                        <li><a class="slide-item" href="#">Members</a></li>
                         <ul class="slide-menu">
                             <li><a class="slide-item" href="{{route('user_index',['u_type' => 'Member'])}}">Current Members</a></li>
                             <li><a class="slide-item" href="#">Summary</a></li>
                         </ul>
                         <li><a class="slide-item" href="#">Employees</a></li>
-                                <ul class="slide-menu">
-                                    <li><a class="slide-item" href="{{route('user_index',['u_type' => 'Employee'])}}">Current Employees</a></li>
-                                    <li><a class="slide-item" href="#">Summary</a></li>
-                                </ul>
-                </ul>
-            </li>
-            <li class="slide">
-                <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-book-open"></i>&nbsp;&nbsp;<span class="side-menu__label">Equipment</span><i class="angle fe fe-chevron-down"></i></a>
-                <ul class="slide-menu">
-                    <li><a class="slide-item" href="{{route('equipment_index')}}">View Equipment</a></li>
-                </ul>
-            </li>
+                        <ul class="slide-menu">
+                            <li><a class="slide-item" href="{{route('user_index',['u_type' => 'Employee'])}}">Current Employees</a></li>
+                            <li><a class="slide-item" href="#">Summary</a></li>
+                        </ul>
+                    </ul>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item text-white" data-toggle="slide" href="#"><i class="fe fe-book-open"></i>&nbsp;&nbsp;<span class="side-menu__label">Equipment</span><i class="angle fe fe-chevron-down"></i></a>
+                    <ul class="slide-menu">
+                        <li><a class="slide-item" href="{{route('equipment_index')}}">View Equipment</a></li>
+                    </ul>
+                </li>
+            @endif
             <li class="slide">
                 <a class="side-menu__item text-white" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><span><i class="bx bx-log-out"></i></span>&nbsp;&nbsp;<span
-                        class="side-menu__label">Sign Out</span></a>
-                        <form id="logout-form" action="{{url('/logout')}}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-            </li>
+                    class="side-menu__label">Sign Out</span></a>
+                    <form id="logout-form" action="{{url('/logout')}}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
 
         </ul>
     </div>
